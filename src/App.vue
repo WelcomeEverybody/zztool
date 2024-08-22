70ab1c73e52c95458170e810171ccc08f867c643
Czhangzihao <101560703+Czhangzihao@users.noreply.github.com>
2024-08-22 11:26:29 +0800

    Updates

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+const zztool: any = inject('$ZZTOOL');
+console.log(zztool.getDateType(new Date()));
+
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ZZTOOL from "../lib/index"
+
+// 挂在全局
+
+
+const app = createApp(App);
+app.provide("$ZZTOOL",new ZZTOOL())
+app.mount('#app')
