a6b16083da7dd7bf0c2e135b6b6aa0af4cb341d2
Czhangzihao <101560703+Czhangzihao@users.noreply.github.com>
2024-09-05 14:39:40 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import ZZTOOL from "../lib/index";
-// 挂在全局
-const app = createApp(App);
-app.provide("$ZZTOOL", new ZZTOOL());
-app.mount('#app');
